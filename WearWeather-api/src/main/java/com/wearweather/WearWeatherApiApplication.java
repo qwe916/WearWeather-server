134696e75254e634c3d124fcc231d45ffb32cb09
hongyoung <qwe916@naver.com>
2023-06-19 21:55:27 +0900

    [Setting] 프로젝트 생성

---
+++ WearWeather-api/src/main/java/com/wearweather/WearWeatherApiApplication.java
@@ -0,0 +1,12 @@
+package com.wearweather;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WearWeatherApiApplication {
+
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(WearWeatherApiApplication.class, args);
+    }
+
+}
