98c86b5b3ecf61ef6ab4e6297ada6ffbb2bb0458
qwe916 <qwe916@naver.com>
2023-06-24 03:55:08 +0900

    [Setting] build.gradle 수정

---
+++ WearWeather-core/src/main/java/com/wearweather/WearWeatherCoreApplication.java
@@ -0,0 +1,13 @@
+package com.wearweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WearWeatherCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WearWeatherCoreApplication.class, args);
+    }
+
+}
